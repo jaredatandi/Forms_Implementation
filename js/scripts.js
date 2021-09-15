8c064f24fefb2aeac7fa050b40348efec359f5cf
jaredatandi <jaredatandi07@gmail.com>
2021-09-15 22:32:01 +0300

    resize screen

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,12 @@ $(function () {
     var exclamationInput = $("input#exclamation").val();
     var verbInput = $("input#verb").val();
     var nounInput = $("input#noun").val();
-    $(".person1").append("bla blah");
-    $(".person2").append("bla blah");
-    $(".animal").append("bla blah");
-    $(".exclamation").append("bla blah");
-    $(".verb").append("bla blah");
-    $(".noun").append("bla blah");
+    $(".person1").append(person1Input);
+    $(".person2").append(person2Input);
+    $(".animal").append(animalInput);
+    $(".exclamation").append(exclamationInput);
+    $(".verb").append(verbInput);
+    $(".noun").append(nounInput);
 
     $("#story").show();
     event.preventDefault();
