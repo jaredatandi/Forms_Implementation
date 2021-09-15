829c7382864d513b1673a44046df08e110761984
jaredatandi <jaredatandi07@gmail.com>
2021-09-15 22:26:17 +0300

    fix value collection from the forms when submitting

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 $(function () {
   $("#blanks form").submit(function () {
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var animalInput = $("input#animal").val();
+    var exclamationInput = $("input#exclamation").val();
+    var verbInput = $("input#verb").val();
+    var nounInput = $("input#noun").val();
     $(".person1").append("bla blah");
     $(".person2").append("bla blah");
     $(".animal").append("bla blah");
@@ -8,5 +14,6 @@ $(function () {
     $(".noun").append("bla blah");
 
     $("#story").show();
+    event.preventDefault();
   });
 });
